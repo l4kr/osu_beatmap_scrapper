b06dd076bcbb5ce087275f5c314c8bbe263a6666
l4kr <lukki411@gmail.com>
2022-10-06 15:21:05 +0200

    bullshi

--- osuscrapur.py
+++ osuscrapur.py
@@ -3,6 +3,5 @@ from bs4 import BeautifulSoup
 
 html_text = requests.get('https://osu.ppy.sh/wiki/en/Tournaments/OWC/2020').text
 soup = BeautifulSoup(html_text, "lxml")
-mappool_div = soup.find(text='https')
-links = mappool_div.findAll('<a>')
+links = soup.findAll('a')
 print(links)
