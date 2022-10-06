209aad8a4f23a0a73db3011f82da409e6c511dc1
l4kr <lukki411@gmail.com>
2022-10-06 15:24:01 +0200

    bullshi

--- osuscrapur.py
+++ osuscrapur.py
@@ -4,4 +4,5 @@ from bs4 import BeautifulSoup
 html_text = requests.get('https://osu.ppy.sh/wiki/en/Tournaments/OWC/2020').text
 soup = BeautifulSoup(html_text, "lxml")
 links = soup.findAll('a')
-print(links)
+for link in links:
+    print(link['href'])
