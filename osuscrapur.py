7e9bb006ce8270641b45801001f10285dca49781
l4kr <lukki411@gmail.com>
2022-10-07 14:48:41 +0200

    bullshi

--- osuscrapur.py
+++ osuscrapur.py
@@ -1,11 +1,15 @@
+from http.cookiejar import CookieJar
 import requests
-import webbrowser
+import browser_cookie3
 from bs4 import BeautifulSoup
 
 owc2020 = 'https://osu.ppy.sh/wiki/en/Tournaments/OWC/2020'
+#CookieJar - browser_cookie3.firefox()
+#r = requests.get(owc2020, cookies=Cookiejar)
 html_text = requests.get(owc2020).text
 soup = BeautifulSoup(html_text, "lxml")
 links = soup.findAll('a')
 for link in links:
-    print(link['href'])
-
+    link = link['href']
+    if "beatmapsets/" in link:
+        print(link)
